511cd68f5cf56703e8a18011feb0c54c4e7546a4
Nathaniel <chaneyna@gmail.com>
2016-08-17 09:00:45 -0400

    Changed the name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #Write the libraries
 from setuptools import setup
 #from setuptools import setup,find_packages
-setup(name='neuroman',
+setup(name='neurotheta',
       version=0.1,
-      description='Neuroman',
-      url='http://github.com/chaneyn/neuroman',
+      description='NeuroTheta',
+      url='http://github.com/chaneyn/neurotheta',
       author='Nathaniel W. Chaney',
       author_email='nchaney@princeton.edu',
       license='MIT',
-      packages=['neuroman',],
-      package_data={'neuroman':['*.txt'],},
+      packages=['neurotheta',],
+      package_data={'neurotheta':['*.txt'],},
       include_package_data=True,
       )
